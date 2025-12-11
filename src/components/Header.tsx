7f5c4dfe318c8a446baafc456cb11be4a7703fcf
neonwatty <jermwatt@gmail.com>
2025-12-11 13:42:06 -0700

    style: change tour button from icon to text label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,10 +59,7 @@ export function Header({ onLogoClick, onShowHelp, onStartTour }: HeaderProps) {
             onClick={onStartTour}
             title="Take a tour of TableCraft"
           >
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" width="14" height="14">
-              <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/>
-              <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
-            </svg>
+            Tour
           </button>
         )}
         {onShowHelp && (
